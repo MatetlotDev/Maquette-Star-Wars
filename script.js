d6cb00c77c53f0cd8e2f0bd4141206c4fa9e3fc9
Matthias Lechien <matthias.lechien@gmail.com>
2022-02-02 10:36:14 +0100

    basic animation

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const arrowRight = document.querySelector('.arrows :nth-child(2)');
+const arrowLeft = document.querySelector('.arrows :nth-child(1)');
 const logos = document.querySelector('.logos');
 
+
 arrowRight.onclick = () => {
-    logos.classList.add('moveLeft');
-    logos.children[0].classList.add('disapearLeft');
-    logos.children[1].classList.add('appearLeft');
+    disappear('left', logos.children[0])
+    appear('right', logos.children[1])
+}
+
+arrowLeft.onclick = () => {
+    appear('left', logos.children[0]);
+    disappear('right', logos.children[1]);
+}
+
+
+function disappear (direction, elem) {
+    elem.className = '';
+    if(direction == 'left') elem.classList.add('disappearLeft')
+    else elem.classList.add('disappearRight')
+}
+
+function appear (direction, elem) {
+    elem.className = ''
+    if(direction == 'left') elem.classList.add('appearLeft')
+    else elem.classList.add('appearRight')
 }
